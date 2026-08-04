35aa65eeae9ae1b9ed10267b855ff42d04fbe7e0
Rahul <2137518@cognizant.com>
2026-08-04 16:51:26 +0530

    updating path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -141,7 +141,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/',
             },
           ],
         },
